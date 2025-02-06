0b104e2554bc6fc8238d53dc8635784dc26ef425
Elie309 <elie.saade432@gmail.com>
2025-02-07 01:14:06 +0200

    bump version to 1.0.0 and integrate @elie309/bookbrowsinglibrary

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
+import { Book } from "@elie309/bookbrowsinglibrary";
+import { useEffect } from "react"
 
 function App() {
 
+  const fetchFunction = () => {
+    let book = new Book("The Great Gatsby", "F. Scott Fitzgerald", "1925", []);
+    console.log(book.title)
+  }
+
+  useEffect(() => {
+    console.log("Hello World!")
+    fetchFunction()
+  }, [])
   return (
     <>
       <p>Hello World!</p>
